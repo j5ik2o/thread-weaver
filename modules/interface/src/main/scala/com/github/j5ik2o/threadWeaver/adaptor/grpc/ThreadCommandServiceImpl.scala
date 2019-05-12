80d1ba7978da53f5d544067c14e7d621c0cffc08
Junichi Kato <j5ik2o@gmail.com>
2019-05-12 11:14:33 +0900

    wip

---
+++ modules/interface/src/main/scala/com/github/j5ik2o/threadWeaver/adaptor/grpc/ThreadCommandServiceImpl.scala
@@ -0,0 +1,42 @@
+package com.github.j5ik2o.threadWeaver.adaptor.grpc
+
+import akka.actor.typed.ActorSystem
+import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.typed.scaladsl.ActorMaterializer
+import cats.data.NonEmptyList
+import com.github.j5ik2o.threadWeaver.adaptor.error.InterfaceError
+import com.github.j5ik2o.threadWeaver.adaptor.grpc.presenter.CreateThreadPresenter
+import com.github.j5ik2o.threadWeaver.adaptor.grpc.validator.ThreadValidateUtils._
+import com.github.j5ik2o.threadWeaver.useCase.CreateThreadUseCase
+import wvlet.airframe.bind
+
+import scala.concurrent.Future
+
+trait ThreadsCommandServiceImpl extends ThreadsCommandService {
+
+  private implicit val system       = bind[ActorSystem[Nothing]]
+  private val createThreadUseCase   = bind[CreateThreadUseCase]
+  private val createThreadPresenter = bind[CreateThreadPresenter]
+
+  implicit val mat = ActorMaterializer()
+
+  private val toErrorResponse = { f: NonEmptyList[InterfaceError] =>
+    Future.successful(CreateThreadResponse(isSuccessful = false, "", f.map(_.message).toList))
+  }
+
+  override def createThread(in: CreateThreadRequest): Future[CreateThreadResponse] = {
+    validateCreateThreadRequest(in)
+      .map { request =>
+        Source
+          .single(request)
+          .via(createThreadUseCase.execute)
+          .via(createThreadPresenter.response)
+          .runWith(Sink.head)
+      }.valueOr(toErrorResponse)
+  }
+
+  override def destroyThread(in: DestroyThreadRequest): Future[DestroyThreadResponse] = ???
+
+  override def addMessages(in: AddMessagesRequest): Future[AddMessagesResponse] = ???
+
+}
--- modules/interface/src/main/scala/com/github/j5ik2o/threadWeaver/adaptor/grpc/ThreadsServiceImpl.scala
+++
@@ -1,9 +0,0 @@
-package com.github.j5ik2o.threadWeaver.adaptor.grpc
-
-import scala.concurrent.Future
-
-class ThreadsServiceImpl extends ThreadsService {
-  override def createThread(in: CreateThreadRequest): Future[CreateThreadResponse]    = ???
-  override def destroyThread(in: DestroyThreadRequest): Future[DestroyThreadResponse] = ???
-  override def addMessages(in: AddMessagesRequest): Future[AddMessagesResponse]       = ???
-}
