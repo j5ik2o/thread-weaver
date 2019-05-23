3b0f9185ff85564ddc4d43e1f23564e6aba65c1e
Junichi Kato <j5ik2o@gmail.com>
2019-05-24 02:21:50 +0900

    wip

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-eval $(minikube docker-env)
 sbt api-server/docker:publishLocal
 helm install ./k8s/thread-weaver-api-server --namespace thread-weaver
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+kubectl create namespace thread-weaver
+kubectl create serviceaccount thread-weaver
+helm init
+
