4197a12b59788395b0ae912e77624d75f7a51402
Junichi Kato <j5ik2o@gmail.com>
2019-05-25 07:45:40 +0900

    wip

--- api-server/src/main/scala/com/github/j5ik2o/threadWeaver/api/ClusterWatcher.scala
+++ api-server/src/main/scala/com/github/j5ik2o/threadWeaver/api/ClusterWatcher.scala
@@ -4,7 +4,7 @@ import akka.actor.{ Actor, ActorLogging }
 import akka.cluster.Cluster
 
 class ClusterWatcher extends Actor with ActorLogging {
-  val cluster = Cluster(context.system)
+  private val cluster = Cluster(context.system)
 
   override def receive: PartialFunction[Any, Unit] = {
     case msg => log.info(s"Cluster ${cluster.selfAddress} >>> " + msg)
