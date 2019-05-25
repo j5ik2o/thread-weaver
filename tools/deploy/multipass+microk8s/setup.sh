d679b8812afb7b01b72a03c9c191e143dc94ba58
Junichi Kato <j5ik2o@gmail.com>
2019-05-25 09:06:08 +0900

    wip

---
+++ tools/deploy/multipass+microk8s/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# https://qiita.com/mumoshu/items/6ff56badcfabe5ab1f49
+# https://nnao45.hatenadiary.com/entry/2018/12/20/085706
+
+multipass launch --name mircok8s --mem 4G --disk 40G --cpus 2
