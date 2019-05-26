7263c44396a59f1931e1c9bac0c9cc2d996486c0
Junichi Kato <j5ik2o@gmail.com>
2019-05-26 11:35:40 +0900

    refactor

--- tools/deploy/minikube/start-minikube.sh
+++ tools/deploy/minikube/start-minikube.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DRIVER="--vmdriver virtualbox"
+
 while getopts d: OPT
 do
     case ${OPT} in
@@ -7,4 +9,4 @@ do
     esac
 done
 
-minikube start ${DRIVER} --kubernetes-version v1.12.8 --cpus 8 --memory 10000 --disk-size 30g
+minikube start ${DRIVER} --kubernetes-version v1.12.8 --cpus 6 --memory 5000 --disk-size 30g
