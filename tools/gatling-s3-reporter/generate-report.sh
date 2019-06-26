1b03f764014c20e2a738246729383f9f1cfc008e
Junichi Kato <j5ik2o@gmail.com>
2019-06-26 17:29:31 +0900

    gatling reporter

--- tools/gatling-s3-reporter/generate-report.sh
+++ tools/gatling-s3-reporter/generate-report.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [[ -z ${S3_GATLING_BUCKET_NAME} ]]; then
+if [ -z $S3_GATLING_BUCKET_NAME ]; then
   echo "env S3_GATLING_BUCKET_NAME does not exist"
   exit 1
 fi
 
-if [[ -z ${S3_GATLING_RESULT_DIR_PATH} ]]; then
+if [ -z $S3_GATLING_RESULT_DIR_PATH ]; then
   echo "env S3_GATLING_RESULT_DIR_PATH does not exist"
   exit 1
 fi
 
-mkdir -p ${S3_GATLING_RESULT_DIR_PATH}
+mkdir -p $S3_GATLING_RESULT_DIR_PATH
 
 # copy logs from s3
 /usr/bin/aws s3 cp s3://${S3_GATLING_BUCKET_NAME}/${S3_GATLING_RESULT_DIR_PATH}/ ${GATLING_HOME}/results/${S3_GATLING_RESULT_DIR_PATH} --recursive --exclude "*" --include "*.log"
