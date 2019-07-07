64f551aa1d21893f9ac02d0fa70b6961cca45f36
Junichi Kato <j5ik2o@gmail.com>
2019-07-07 23:19:40 +0900

    fix bugs.

--- tools/deploy/k8s-d4m/migrate-local-db.sh
+++ tools/deploy/k8s-d4m/migrate-local-db.sh
@@ -3,10 +3,9 @@
 set -eu
 
 cd $(dirname $0)
-HOST=$(minikube ip)
+HOST=localhost
 pushd ../../../
 
-sbt -Dmysql.host="$HOST" -Dmysql.port=30306 'migrate-mysql/run'
 DYNAMODB_HOST="$HOST" DYNAMODB_PORT=32000 sbt 'migrate-dynamodb/run'
 
 popd
