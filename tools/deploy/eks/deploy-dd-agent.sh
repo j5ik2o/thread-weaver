c16414d6f975ce875cdbf0ceb7c1e9d97c315847
Junichi Kato <j5ik2o@gmail.com>
2019-07-13 16:08:47 +0900

    update namespace.

--- tools/deploy/eks/deploy-dd-agent.sh
+++ tools/deploy/eks/deploy-dd-agent.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm upgrade dd-agent --install -f dd-agent-values.yaml --namespace gaudi-poc stable/datadog --recreate-pods
+helm upgrade dd-agent --install -f dd-agent-values.yaml --namespace thread-weaver stable/datadog --recreate-pods
