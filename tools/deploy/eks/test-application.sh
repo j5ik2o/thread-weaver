ef946e6bfc74f156409895230b97fcaf04736002
Junichi Kato <j5ik2o@gmail.com>
2019-07-20 16:04:02 +0900

    refactor response

--- tools/deploy/eks/test-application.sh
+++ tools/deploy/eks/test-application.sh
@@ -24,7 +24,7 @@ ACCOUNT_ID=01DB5QXD4NP0XQTV92K42B3XBF
 ADMINISTRATOR_ID=01DB5QXD4NP0XQTV92K42B3XBF
 
 THREAD_ID=$(curl -v -X POST "http://$API_HOST:$API_PORT/v1/threads/create" -H "accept: application/json" -H "Content-Type: application/json" \
-    -d "{\"accountId\":\"${ACCOUNT_ID}\",\"title\":\"string\",\"remarks\":\"string\",\"administratorIds\":[\"${ADMINISTRATOR_ID}\"],\"memberIds\":[\"${ACCOUNT_ID}\"],\"createAt\":10000}" | jq -r .threadId)
+    -d "{\"accountId\":\"${ACCOUNT_ID}\",\"title\":\"string\",\"remarks\":\"string\",\"administratorIds\":[\"${ADMINISTRATOR_ID}\"],\"memberIds\":[\"${ACCOUNT_ID}\"],\"createAt\":10000}" | jq -r .thread_id)
 echo "THREAD_ID=$THREAD_ID"
 
 if [[ -z "${THREAD_ID}" ]]; then
